6b3a05bbdf8b2b8801278110dc8ce1c4a8e96880
DraftMan <nicovanaarsen@gmail.com>
2020-06-17 13:06:58 +0200

    logs updated

--- src/rest/RequestHandler.js
+++ src/rest/RequestHandler.js
@@ -148,7 +148,7 @@ class RequestHandler {
     } else if (res.status === 429) {
       // A ratelimit was hit - this should never happen
       this.queue.unshift(item);
-      console.error(`429 hit on route ${item.request.route}`, item.request);
+      console.error(`429 hit on route ${item.request.route}`, item.request.options);
       await Util.delayFor(this.retryAfter);
       return this.run();
     } else if (res.status >= 500 && res.status < 600) {
